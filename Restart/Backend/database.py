5daea8745c0f0bf60c42c22e3252c298d14c04c3
Simon <simon.muscas@gmail.com>
2023-03-13 16:25:05 +0100

    set up remote Database

--- Restart/Backend/database.py
+++ Restart/Backend/database.py
@@ -1,1 +1,26 @@
-# TODO: here I will create the basic database structure with prisma and create the connection to the database
+import asyncio
+from prisma import Prisma
+# THIS IS AN EXAMPLE SCRIPT FOR INTERACTING WITH THE DB
+# https://prisma-client-py.readthedocs.io/en/stable/getting_started/quickstart/
+async def main() -> None:
+    db = Prisma()
+    await db.connect()
+
+    post = await db.post.create(
+        {
+            'title': 'Hello from prisma!',
+            'desc': 'Prisma is a database toolkit and makes databases easy.',
+            'published': True,
+        }
+    )
+    print(f'created post: {post.json(indent=2, sort_keys=True)}')
+
+    found = await db.post.find_unique(where={'id': post.id})
+    assert found is not None
+    print(f'found post: {found.json(indent=2, sort_keys=True)}')
+
+    await db.disconnect()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
